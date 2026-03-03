aa6c9ae7d13da97603556e9cf36844a19add2876
marzvk <marianozovak1@gmail.com>
2026-03-03 11:23:07 -0300

    primer commit de la rama /vistas. slots disponibles funciona

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "agenda"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("slots/", views.lista_slots, name="lista_slots"),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from agenda.models import Slot
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the agenda index.")
 
+
+def lista_slots(request):
+
+    slots = Slot.objects.filter(disponible=True).order_by("fecha", "hora_inicio")
+
+    return render(request, "agenda/lista_slots.html", {"slots": slots})
