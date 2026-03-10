376d4fb48687b7d38b7fed6d38a36262414a892d
marzvk <marianozovak1@gmail.com>
2026-03-10 17:11:30 -0300

    commit inicial rama notificaciones, armando los archivos y carpetas

---
+++ config/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+
+# Le dice a Celery dónde encontrar la configuración de Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+app = Celery("medagenda")
+
+# Lee toda la configuración de Celery desde settings.py
+# El namespace "CELERY" significa que en settings todas las
+# variables de Celery empiezan con CELERY_
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Autodescubre tasks.py en todas las apps instaladas
+app.autodiscover_tasks()
